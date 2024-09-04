bf381a7e6114e5188cd4d07bc6f964b41778f166
leninner <mazabandalenin180@gmail.com>
2024-09-03 19:39:33 -0500

    feat: initial configuration for wezterm

---
+++ config/launch.lua
@@ -0,0 +1,22 @@
+local platform = require('utils.platform')()
+
+local options = {
+   default_prog = {},
+   launch_menu = {},
+}
+
+if platform.is_mac then
+   options.default_prog = { '/opt/homebrew/bin/zsh' } -- check when I move to M1
+   options.launch_menu = {
+      { label = 'Bash', args = { 'bash', '-l' } },
+      { label = 'Zsh', args = { 'zsh', '-l' } },
+   }
+elseif platform.is_linux then
+   options.default_prog = { 'zsh' }
+   options.launch_menu = {
+      { label = 'Bash', args = { 'bash', '-l' } },
+      { label = 'Zsh', args = { 'zsh', '-l' } },
+   }
+end
+
+return options
